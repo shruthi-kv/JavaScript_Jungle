ec696fd4985c6a7a1c33ac379b8a02a9a3e56ec2
Shruthi KV <88342731+shruthi-kv@users.noreply.github.com>
2025-08-02 22:57:51 +0530

    Practise (#5)
    * call
    
    * apply
    
    * bind
    
    * rest
    
    * spread
    
    ---------
    
    Co-authored-by: Shruthi KV <shruthikv@Mac.lan>

---
+++ Arrays/rest.js
@@ -0,0 +1,5 @@
+function printAll(...arguments){
+    console.log(arguments.length);
+}
+
+printAll(1, 2, 3, 4, 5); // Outputs: 5
---
+++ Arrays/spread.js
@@ -0,0 +1,5 @@
+const a = [1, 2, 3, 4, 5];
+const b = [6, 7, 8, 9, 10];
+
+const combined = [...a,...b]
+console.log(combined)
